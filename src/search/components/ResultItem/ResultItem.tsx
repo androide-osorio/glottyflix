f5e442c7800f48366fb96b3d0c33d1a5fa907d76
Andres Osorio <androideosorio@me.com>
2019-07-15 11:35:52 -0500

    adjust search result styles

--- src/search/components/ResultItem/ResultItem.tsx
+++ src/search/components/ResultItem/ResultItem.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import classes from './ResultItem.module.css'
 
 const ResultItem = ({ title, image }: any) => {
   return (
-    <div className="ResultItem">
+    <div className={classes.ResultItem}>
       <img src={image} alt={`${title} poster`}/>
-      <h3>{title}</h3>
+      <h3 className={classes.ResultItem__title}>{title}</h3>
     </div>
   );
 };
--- src/search/components/SearchResults/SearchResults.tsx
+++ src/search/components/SearchResults/SearchResults.tsx
@@ -3,13 +3,15 @@ import { useSelector } from 'react-redux';
 import ResultItem from '../ResultItem/ResultItem'
 import { selectPosterPath } from '../../../tmdb/store';
 
+import classes from './SearchResults.module.css'
+
 const SearchResults = ({ items }: any) => {
   const posterPath = useSelector(selectPosterPath('w185') as any)
 
   return (
-    <ul>
+    <ul className={classes.SearchResults}>
       {items.map((item: any) =>
-        <li key={item.id}>
+        <li className={classes.SearchResults__item} key={item.id}>
           <ResultItem title={item.original_name} image={`${posterPath}${item.poster_path}`} />
         </li>
       )}
